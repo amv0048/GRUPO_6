04c1657f382d0f80fac77133e2c4a97dfc2e2295
sgm0044 <sgm0044@alu.medac.es>
2026-03-18 12:48:02 +0100

    Añadido errores Php

---
+++ public/js/validacionPhP.js
@@ -0,0 +1,40 @@
+
+window.onload = () =>{
+    var params = new URLSearchParams(window.location.search);
+    const error = params.get("error");
+    let DescError = "";
+
+    if(error){
+        switch (error) {
+            case "nombre":
+                DescError = "No te saltes el front"
+                break;
+            case "apellido":
+                DescError = ""
+                break;
+            case "nombre":
+                DescError = ""
+                break;
+            case "email":
+                DescError = ""
+                break;
+            case "pass":
+                DescError = ""
+                break;
+            case "db":
+                DescError = "Ha ocurrido un error, No se ha podido registrar el usuario en la bbdd"
+                break;
+            default:
+                break;
+        }
+    }
+
+
+
+
+    let div = document.createElement("div")
+    div.setAttribute("class" , "error")
+    div.textContent = DescError;
+
+    document.getElementById("estructura").prepend(div)
+}
