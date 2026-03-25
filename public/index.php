ed6c70bb8bc1c8516bee72ac2bcaba0c237b7faa
sgm0044 <sgm0044@alu.medac.es>
2026-03-25 14:02:34 +0100

    index header

--- public/index.php
+++ public/index.php
@@ -5,8 +5,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>De momento</title>
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/registro.css">
 </head>
 <body>
+
+    <header>
+    <!--Logo que rederige al index-->
+        <nav class="hBotones">
+            <a class="hBoton" href="" target="_self">PROTECTORAS</a>
+            <a class="hBoton" href="" target="_self">COLABORADORES</a>
+        </nav>
+        <nav id="header-izq">
+            <a href="index.html" target="_self">
+            <img src="" alt="logo" 
+             width=""> <!--Todo esto está en blanco hasta que tengamos el logo listo y el index-->
+        </a>
+        </nav>
+        
+        <nav class="hBotones"> 
+            <a class="hBoton" href="" target="_self">URGENTE</a>
+            <a class="hBoton" href="registro.html" target="_self">REGISTRATE!</a>
+            <a  href="" target="_self" id="boton-destacado">INICIA SESIÓN</a>
+
+        </nav>
+    </header>
+    <br><br><br>
 
     <p>Tienes Cuenta ? <a href="login.html">Logearse</a></p>
     <p>No tienes Cuenta ? <a href="registro.html">Registrate</a></p>
