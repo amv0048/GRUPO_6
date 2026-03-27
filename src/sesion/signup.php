a81c3186d53acf829c317864bbb4a8b6054b7ac0
sgm0044 <sgm0044@alu.medac.es>
2026-03-27 11:37:34 +0100

    !!

--- src/sesion/signup.php
+++ src/sesion/signup.php
@@ -3,7 +3,7 @@
 require '../PHPMailer.php';
 require '../SMTP.php';
 require '../Exception.php';
-require '../config.php';
+require '../config1.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
